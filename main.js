bac28756baf004661c8ec6bdb3b2959f233f0679
Shamil Omarov <s_omarov05@mail.ru>
2021-12-14 17:57:51 +0300

    Создал ф-ю чтоб кружочек сам бегал по кругу )

--- main.js
+++ main.js
@@ -83,3 +83,17 @@ function getRandomNumber(min, max) {
 function getRandomColor() {
     return colors[Math.floor(Math.random() * colors.length)];
 }
+
+function winTheGame() {
+    function kill() {
+        const circle = document.querySelector('.circle')
+
+        if(circle){
+            circle.click()
+        }
+    }
+    
+    setInterval(kill, 99)
+}
+// winTheGame() 
+// чтоб кружочек сам бегал по кругу ))))
